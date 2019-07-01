7ff262c66e44a92bd12dd91329c6e6dc07497479
Trang Do <quynhtrangdo94@gmail.com>
2019-07-01 10:59:37 -0400

    Added type, properties, methods for learning purposed

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -9,5 +9,37 @@ import { Component } from '@angular/core';
 })
 
 export class UserComponent {
+  // *** PROPERTIES ***
+  firstName: string;
+  lastName: string;
+  age: number;
+  address;
+  hasKids: false;
+  numberArray: number[];
+  myTuple: [string , number, boolean];
+  // the var need to be array of the first 3 in order string, num, bool. After that, it can be whatever
 
+  // type NULL, UNDEFINED and VOID can be manipulated or 'reassigned'
+
+  // *** METHODS ***
+  constructor() {
+    this.firstName = 'John';
+    this.lastName = 'Doe'
+    this.age = 30;
+    this.address = {
+      street: '50 Main St',
+      city: 'Boston',
+      state: 'MA'
+    }
+
+    this.sayHello()
+  }
+
+  sayHello(): void {
+    console.log(`Hello ${this.firstName}`)
+  }
+
+  addNumber(num1: number, num2: number): number {
+    return num1 + num2;
+  }
 }
