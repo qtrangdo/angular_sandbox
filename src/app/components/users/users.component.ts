56a91b779e0c8071216d270c35abadf1be7f5e14
Trang Do <quynhtrangdo94@gmail.com>
2019-07-01 12:29:13 -0400

    Used ngFor and ngIf

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +7,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
+  showExtended: boolean = false;
 
-  constructor() { }
+  constructor () { }
 
   ngOnInit() {
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 30,
+        address: {
+          street: '50 Main St',
+          city: 'Boston',
+          state: 'MA'
+        }
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Johnson',
+        age: 24,
+        address: {
+          street: '123 Rock Dr',
+          city: 'New York',
+          state: 'NY'
+        }
+      },
+      {
+        firstName: 'Sarah',
+        lastName: 'Hanns',
+        age: 27,
+        address: {
+          street: '23527 GwenStepple Ave',
+          city: 'Huston',
+          state: 'TX'
+        }
+      }
+    ];
+
+    this.addUser({
+      firstName: 'Tom',
+      lastName: 'Fills',
+    })
   }
 
+  addUser(user: User): void {
+    this.users.push(user)
+  }
 }
--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,10 +1,12 @@
 export interface User {
   firstName: string,
   lastName: string,
-  age: number,
-  address: {
-    street: string,
-    city: string,
-    state: string
+  age?: number,
+  address?: {
+    street?: string,
+    city?: string,
+    state?: string
   }
 }
+
+// "?" means OPTIONAL
