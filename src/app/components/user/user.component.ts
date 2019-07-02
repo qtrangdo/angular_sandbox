2babe5d334a91f959ede60cdc563bdf053735545
Trang Do <quynhtrangdo94@gmail.com>
2019-07-02 15:06:32 -0400

    Fixed compile error

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -18,12 +18,13 @@ export class UserComponent implements OnInit{
     this.user = {
       firstName: 'John',
       lastName: 'Doe',
-      age: 30,
-      address: {
-        street: '50 Main St',
-        city: 'Boston',
-        state: 'MA'
-      }
+      email: 'john@text.com'
+      // age: 30,
+      // address: {
+      //   street: '50 Main St',
+      //   city: 'Boston',
+      //   state: 'MA'
+      // }
     }
   }
 }
